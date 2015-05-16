6354f3bb75e5ce5a86eb1f52337f40e0307a430e
Ravi Sharma <ravis4376@gmail.com>
2015-05-16 11:01:13 +0530

    added fragments to product details

--- app/src/main/java/bootcamp/android/activities/ProductDetailsActivity.java
+++ app/src/main/java/bootcamp/android/activities/ProductDetailsActivity.java
@@ -1,23 +1,16 @@
 package bootcamp.android.activities;
 
-import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import androidplugins.Callback;
-import androidplugins.imagefetcher.ImageFetcher;
 import bootcamp.android.R;
-import bootcamp.android.models.Product;
-
-import static bootcamp.android.constants.Constants.*;
+import bootcamp.android.fragments.ProductDetailsFragment;
 
 
-public class ProductDetailsActivity extends Activity {
+public class ProductDetailsActivity extends FragmentActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,30 +18,12 @@ public class ProductDetailsActivity extends Activity {
     setContentView(R.layout.product_details_container);
 
     Bundle extraArguments = getIntent().getExtras();
-    ViewGroup parent = (ViewGroup) findViewById(R.id.product_details_container);
-    parent.addView(getPopulatedView(getLayoutInflater(), parent, extraArguments));
-  }
 
-  private View getPopulatedView(LayoutInflater layoutInflater, ViewGroup parent, Bundle extraArguments) {
-    View productDetailsView = layoutInflater.inflate(R.layout.product_details, parent, false);
-    Product product = extraArguments.getParcelable(PRODUCT_KEY);
-    TextView imageTitle = (TextView) productDetailsView.findViewById(R.id.product_title);
-    imageTitle.setText(product.getTitle());
-    ImageView imageView = (ImageView) productDetailsView.findViewById(R.id.product_image);
-    ImageFetcher imageFetcher = new ImageFetcher(bitmapCallback(imageView), this);
-    imageFetcher.execute(product.getImageUrl());
-    TextView issueDescription = (TextView) productDetailsView.findViewById(R.id.product_description);
-    issueDescription.setText(product.getDescription());
-    return productDetailsView;
-  }
-
-  private Callback<Bitmap> bitmapCallback(final ImageView imageView) {
-    return new Callback<Bitmap>() {
-
-      @Override
-      public void execute(Bitmap object) {
-        imageView.setImageBitmap(object);
-      }
-    };
+    FragmentManager fragManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragManager.beginTransaction();
+    ProductDetailsFragment fragment = new ProductDetailsFragment();
+    fragment.setArguments(extraArguments);
+    fragmentTransaction.add(R.id.product_details_container, fragment);
+    fragmentTransaction.commit();
   }
 }
---
+++ app/src/main/java/bootcamp/android/fragments/ProductDetailsFragment.java
@@ -0,0 +1,46 @@
+package bootcamp.android.fragments;
+
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidplugins.Callback;
+import androidplugins.imagefetcher.ImageFetcher;
+import bootcamp.android.R;
+import bootcamp.android.models.Product;
+
+import static bootcamp.android.constants.Constants.PRODUCT_KEY;
+
+public class ProductDetailsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View productDetailsView = inflater.inflate(R.layout.product_details, container, false);
+        Product product = getArguments().getParcelable(PRODUCT_KEY);
+        TextView imageTitle = (TextView) productDetailsView.findViewById(R.id.product_title);
+        imageTitle.setText(product.getTitle());
+        ImageView imageView = (ImageView) productDetailsView.findViewById(R.id.product_image);
+        ImageFetcher imageFetcher = new ImageFetcher(bitmapCallback(imageView), this.getActivity());
+        imageFetcher.execute(product.getImageUrl());
+        TextView issueDescription = (TextView) productDetailsView.findViewById(R.id.product_description);
+        issueDescription.setText(product.getDescription());
+        return productDetailsView;
+    }
+
+    private Callback<Bitmap> bitmapCallback(final ImageView imageView) {
+        return new Callback<Bitmap>() {
+
+            @Override
+            public void execute(Bitmap object) {
+                imageView.setImageBitmap(object);
+            }
+        };
+    }
+}
